d96a7d938e6630b9bda316d3b78da8aaf2452279
Alex1966design <a.kamenetsky@mail.ru>
2025-03-25 11:15:38 +0300

    Update app.py
    fix: обновлён app.py для Render

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Привет, Flask работает! 🎉'
+def index():
+    return "Привет от Flask на Render! 🚀"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+
+
+
+
 
